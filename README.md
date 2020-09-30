5a925b4caa09305fbc5101b412580621911d28ce
tander77 <32040125+tander77@users.noreply.github.com>
2020-09-30 09:05:36 -0400

    Initial commit
    Created hello.py, index.html, user.html, added picture submission, and readme file

---
+++ README.md
@@ -0,0 +1,8 @@
+# Torin Anderson
+
+this repo is a clone of
+https://github.com/miguelgrinberg/flasky
+
+# Activity 1 Screenshot
+
+![picture](Activity1.jpg)
---
+++ hello.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from datetime import datetime
+from flask_moment import Moment
+app = Flask(__name__)
+
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html', current_time=datetime.utcnow())
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html',name=name, current_time=datetime.utcnow())
+
+if __name__ == '__main__':
+    app.run(debug=True)
