ee678a3d1383d3343e31d8e3d58d3d52fef91554
tander77 <32040125+tander77@users.noreply.github.com>
2020-09-30 12:15:37 -0400

    Updated readme, hello.py and index.html
    Added functionality to register a user's email address and check if it is a valid uoft email. Added submission pictures for Activity2 into the readme

--- README.md
+++ README.md
@@ -6,3 +6,13 @@ https://github.com/miguelgrinberg/flasky
 # Activity 1 Screenshot
 
 ![picture](Activity1.jpg)
+
+# Activity 2 Screenshots
+
+![picture](Activity2Part1.jpg)
+
+![picture](Activity2Part2.jpg)
+
+![picture](Activity2Part3.jpg)
+
+![picture](Activity2Part4.jpg)
--- hello.py
+++ hello.py
@@ -4,27 +4,35 @@ from datetime import datetime
 from flask_moment import Moment
 from flask_wtf import Form
 from wtforms import StringField, SubmitField
-from wtforms.validators import Required
+from email_validator import EmailNotValidError
+from wtforms.validators import Required, Email, Regexp
+import re
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'something'
+app.config['SECRET_KEY'] = 'newthing'
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 class NameForm(Form):
     name = StringField('What is your name?', validators=[Required()])
+    email = StringField('What is your UofT email?', validators=[Required(), Email()])
     submit = SubmitField('Submit')
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
     if form.validate_on_submit():
         old_name = session.get('name')
+        old_email = session.get('email')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
         session['name'] = form.name.data
+        session['email'] = form.email.data
         return redirect(url_for('index'))
-    return render_template('index.html', form = form, name = session.get('name'), current_time=datetime.utcnow())
+    return render_template('index.html', form = form, name = session.get('name'), email = session.get('email'), current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
